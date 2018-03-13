d4a4464e8580c157038526810287f7af883ac4e2
Tien Pham <r3dsky02@yahoo.com.vn>
2018-03-14 00:01:17 +0200

    Reverse words

--- kyu-6.js
+++ kyu-6.js
@@ -76,3 +76,14 @@ function findOutlier(integers){
     :
     integers.filter(int => int%2 !== 0)[0]
 }
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+//
+// Example:
+//
+// reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
+
+function reverseWords(str){
+  return str.split(" ").reverse().join(" ")
+}
